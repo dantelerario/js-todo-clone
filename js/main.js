d30fb9cdae89709bfef8a21c00ead4457d515ee4
Dante L'Erario <dantelerario@gmail.com>
2020-04-16 15:57:38 +0200

    JS start
    CSS fixing

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Ricreare la todo list vista oggi durante la lezione (focus dell’esercizio la logica)
+// Usiamo i template in html e il clone per separare la parte logica (JS) dalla parte di markup (HTML)
+// recap funzionalità: mostrare, aggiungere e eliminare elementi dalla lista
+
+$(document).ready( function() {
+
+  var todoList = ['Comprare la pasta', 'Pagare la bolletta della fibra', 'Pulire il bagno', 'Ricordati che devi morire']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); /* END DOCUMENT */
